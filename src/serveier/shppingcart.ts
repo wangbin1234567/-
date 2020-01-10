86dd621ed2a4af3752f335d306ca60083097e4fc
Your Name <you@example.com>
2020-01-10 20:43:56 +0800

    季志敏

--- src/serveier/shppingcart.ts
+++ src/serveier/shppingcart.ts
@@ -1,17 +1,17 @@
 import request from "../utils/request"
 export const cartIndex=()=>{
-    return request('http://169.254.160.213:8888/cart/index')
+    return request('/cart/index')
 }
 export const cartChecked=(data:object)=>{
     return request({
-        url:'http://169.254.160.213:8888/cart/checked',
+        url:'/cart/checked',
         method:"POST",
         data
     })
 }
 export const cartDelete=(removeList:any)=>{
     return request({
-        url:'http://202.96.155.121:8888/api/cart/delete',
+        url:'/api/cart/delete',
         method:"POST",
         data:{
             productIds:removeList
@@ -21,7 +21,7 @@ export const cartDelete=(removeList:any)=>{
 }
 export const cartUpdate=(data:object)=>{
     return request({
-        url:'http://169.254.160.213:8888/api/cart/update',
+        url:'/api/cart/update',
         method:"POST",
       data
     })
